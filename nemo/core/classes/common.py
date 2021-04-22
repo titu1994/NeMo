29a8e7916aaa9c2a32dd86bba3deef238ce3112f
Somshubra Majumdar <titu1994@gmail.com>
2021-04-21 21:36:22 -0700

    Add Multi Node Rank tests
    Signed-off-by: smajumdar <titu1994@gmail.com>

--- nemo/core/classes/common.py
+++ nemo/core/classes/common.py
@@ -15,6 +15,7 @@
 
 """Interfaces common to all Neural Modules and Models."""
 import hashlib
+import inspect
 import traceback
 from abc import ABC, abstractmethod
 from contextlib import contextmanager
@@ -469,7 +470,12 @@ class Serialization(ABC):
                         imported_cls = cls
 
                     try:
-                        instance = imported_cls(cfg=config, trainer=trainer)
+                        accepts_trainer = Serialization._inspect_signature_for_trainer(imported_cls)
+                        if accepts_trainer:
+                            instance = imported_cls(cfg=config, trainer=trainer)
+                        else:
+                            instance = imported_cls(cfg=config)
+
                     except Exception:
                         imported_cls_tb = traceback.format_exc()
                         instance = None
@@ -482,7 +488,11 @@ class Serialization(ABC):
                         f"Falling back to `cls`.\n"
                         f"{imported_cls_tb}"
                     )
-                instance = cls(cfg=config, trainer=trainer)
+                accepts_trainer = Serialization._inspect_signature_for_trainer(cls)
+                if accepts_trainer:
+                    instance = cls(cfg=config, trainer=trainer)
+                else:
+                    instance = cls(cfg=config)
 
         if not hasattr(instance, '_cfg'):
             instance._cfg = config
@@ -506,6 +516,17 @@ class Serialization(ABC):
             raise NotImplementedError(
                 'to_config_dict() can currently only return object._cfg but current object does not have it.'
             )
+
+    @classmethod
+    def _inspect_signature_for_trainer(cls, check_cls):
+        if hasattr(check_cls, '__init__'):
+            signature = inspect.signature(check_cls.__init__)
+            if 'trainer' in signature.parameters:
+                return True
+            else:
+                return False
+        else:
+            return False
 
 
 class FileIO(ABC):
